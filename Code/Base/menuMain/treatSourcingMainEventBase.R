0f6df42e6e02b108c61080f116d3cb187bba877c
ai4ir <smartAI4IR@gmail.com>
2021-01-31 23:33:17 +0900

    treatSourcingMainEventBase.R 추가

---
+++ Code/Base/menuMain/treatSourcingMainEventBase.R
@@ -0,0 +1,92 @@
+treatSourcingMainEventBase <- function(input, output, session) {
+  # 범용 리포트
+  observeEvent(input$renderReportCommonSource, {
+    dfReportCommon <<- DFSource
+    fromReportCommon <<- "_source"
+    showModal(ModalActionButtonsReportCommon())
+  })
+  
+  
+  observeEvent(input$renderReportCommonPlotScatterOneY, {
+    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportScatterOneY.Rmd"
+    curCommonPlot <<- "ScatterOneY"
+    fileNameSuffix <<- "_Scatter_OneY_"
+    aesList[["y"]] <<- NA
+    triggerMCP <<- "commonPlot"
+    showModal(ModalCommonPlot())
+  })
+  
+  observeEvent(input$renderReportCommonPlotScatterOneX, {
+    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportScatterOneX.Rmd"
+    curCommonPlot <<- "ScatterOneX"
+    fileNameSuffix <<- "_Scatter_OneX_"
+    aesList[["x"]] <<- NA
+    triggerMCP <<- "commonPlot"
+    showModal(ModalCommonPlot())
+  })
+  
+  observeEvent(input$renderReportCommonPlotViolinOneY, {
+    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportViolinOneY.Rmd"
+    curCommonPlot <<- "ViolinOneY"
+    fileNameSuffix <<- "_ViolinOneY_"
+    triggerMCP <<- "commonPlot_Violin"
+    # aesListScatter <<- aesList
+    showModal(ModalCommonPlot())
+    hideButton=c( "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id", "MCP_fitOption")
+    for(i in seq_along(hideButton)) {
+      hide(hideButton[i])
+    }
+  })
+  
+  observeEvent(input$renderReportCommonPlotViolinOneX, {
+    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportViolinOneX.Rmd"
+    curCommonPlot <<- "ViolinOneX"
+    fileNameSuffix <<- "_ViolinOneX_"
+    triggerMCP <<- "commonPlot_Violin"
+    # aesListScatter <<- aesList
+    showModal(ModalCommonPlot())
+    hideButton=c( "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id", "MCP_fitOption")
+    for(i in seq_along(hideButton)) {
+      hide(hideButton[i])
+    }
+  })
+  
+  observeEvent(input$renderReportCommonPlotHistogram, {
+    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportHistogram.Rmd"
+    curCommonPlot <<- "Histogram"
+    fileNameSuffix <<- "_Histogram_"
+    aesList[["y"]] <<- NA
+    triggerMCP <<- "commonPlot_Histogram"
+    showModal(ModalCommonPlot())
+    hideButton=c("MCP_y","MCP_color", "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id",
+                 "MCP_fitOption")
+    for(i in seq_along(hideButton)) {
+      hide(hideButton[i])
+    }
+    
+  })
+  
+  observe({
+    treatModalCommonPlot(input, output, session)
+  })
+  
+  observeEvent(input$renderReportCommonDescriptive, {
+    pathFileRmdCommonDescriptive <<- "Base/common/Rmd/commonDescriptiveReport.Rmd"
+    showModal(ModalCommonDescriptive())
+  })
+  
+  observe({
+    treatModalCommonDescriptive(input, output, session)
+  })
+  
+  #변수 처리
+  observeEvent(input$treatVar, {
+    showModal(ModalTreatVar())
+  })
+  
+  observe({
+    treatModalTreatVar(input, output, session)
+  })
+  
+}
+
--- Code/each/menuMain/sourcingMain.R
+++ Code/each/menuMain/sourcingMain.R
@@ -14,8 +14,8 @@ sourcingMainUI <- function() {
         fluidRow(
           column(2, actionButton("renderReportSourcing", "리포트") ),
           includeCSS("Base/www/html/globalModal.css"),
-          column(2, actionButton("renderReportCommonPlot", "탐색 리포트") ),
-          # column(2, actionButton("renderReportCommonSource", "탐색 리포트") ),
+          # column(2, actionButton("renderReportCommonPlot", "탐색 리포트") ),
+          column(2, actionButton("renderReportCommonSource", "탐색 리포트") ),
           column(6, tags$h4(":::::::") ),
           column(2, actionButton("treatVar", "변수 전처리") )
           
@@ -47,6 +47,10 @@ sourcingMainUI <- function() {
 }
 
 sourcingMain <- function(input, output, session) {
+  
+  # 범용 리포트
+  treatSourcingMainEventBase(input, output,session)
+  
   # 맞춤 리포트
   observeEvent(input$renderReportSourcing, {
     # showModal(ModalCheckboxGroup(outputFileNamesSourcingReport, outputFileNamesSourcingReport,
@@ -63,95 +67,7 @@ sourcingMain <- function(input, output, session) {
                          pathFileRmdSourcingReport, pathHTMLReport)
   })
   
-  # 범용 리포트
-  observeEvent(input$renderReportCommonPlot, {
-    dfReportCommon <<- DFSource
-    fromReportCommon <<- "_source"
-    showModal(ModalActionButtonsReportCommon())
-  })
-  
-  observeEvent(input$renderReportCommonPlotScatterOneY, {
-    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportScatterOneY.Rmd"
-    curCommonPlot <<- "ScatterOneY"
-    fileNameSuffix <<- "_Scatter_OneY_"
-    aesList[["y"]] <<- NA
-    triggerMCP <<- "commonPlot"
-    showModal(ModalCommonPlot())
-  })
-  
-  observeEvent(input$renderReportCommonPlotScatterOneX, {
-    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportScatterOneX.Rmd"
-    curCommonPlot <<- "ScatterOneX"
-    fileNameSuffix <<- "_Scatter_OneX_"
-    aesList[["x"]] <<- NA
-    triggerMCP <<- "commonPlot"
-    showModal(ModalCommonPlot())
-  })
-  
-  observeEvent(input$renderReportCommonPlotViolinOneY, {
-    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportViolinOneY.Rmd"
-    curCommonPlot <<- "ViolinOneY"
-    fileNameSuffix <<- "_ViolinOneY_"
-    triggerMCP <<- "commonPlot_Violin"
-    # aesListScatter <<- aesList
-    showModal(ModalCommonPlot())
-    hideButton=c( "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id", "MCP_fitOption")
-    for(i in seq_along(hideButton)) {
-      hide(hideButton[i])
-    }
-  })
-  
-  observeEvent(input$renderReportCommonPlotViolinOneX, {
-    pathFileRmdCommonPlot <<- "Base/common/Rmd/commonPlotReportViolinOneX.Rmd"
-    curCommonPlot <<- "ViolinOneX"
-    fileNameSuffix <<- "_ViolinOneX_"
-    triggerMCP <<- "commonPlot_Violin"
-    # aesListScatter <<- aesList
-    showModal(ModalCommonPlot())
-    hideButton=c( "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id", "MCP_fitOption")
-    for(i in seq_along(hideButton)) {
-      hide(hideButton[i])
-    }
-  })
-  
-  
-  
-  observeEvent(input$renderReportCommonPlotHistogram, {
-    pathFileRmdCommonPlot <<- "Base/Rmd/commonPlotReportHistogram.Rmd"
-    curCommonPlot <<- "Histogram"
-    fileNameSuffix <<- "_Histogram_"
-    aesList[["y"]] <<- NA
-    triggerMCP <<- "commonPlot_Histogram"
-    showModal(ModalCommonPlot())
-    hideButton=c("MCP_y","MCP_color", "MCP_size", "MCP_shape", "MCP_tooltip", "MCP_data_id",
-                 "MCP_bFit")
-    for(i in seq_along(hideButton)) {
-      hide(hideButton[i])
-    }
-  })
-  
-  
-  observe({
-    treatModalCommonPlot(input, output, session)
-  })
-  
-  observeEvent(input$renderReportCommonDescriptive, {
-    pathFileRmdCommonDescriptive <<- "Base/Rmd/commonDescriptiveReport.Rmd"
-    showModal(ModalCommonDescriptive())
-  })
-  
-  observe({
-    treatModalCommonDescriptive(input, output, session)
-  })
 
-  #변수 처리
-  observeEvent(input$treatVar, {
-    showModal(ModalTreatVar())
-  })
-  
-  observe({
-    treatModalTreatVar(input, output, session)
-  })
   
   output$strDFsource <- renderPrint({
     theme_update(axis.title=element_text(size=40))
